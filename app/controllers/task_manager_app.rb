dbe655127f2bfa16213ea1d9fd163472f426ee48
Amara Lovato <amaracuenca@gmail.com>
2017-01-22 10:53:00 -0700

    first commit, adds code before SQL phase

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,24 @@
+require_relative '../models/task.rb'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ['task1', 'task2', 'task3']
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+  end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task
+  def initialize(task_params)
+    @description  = task_params["description"]
+    @title        = task_params["title"]
+  end
+end
