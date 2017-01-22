e08dd8d3ca8954c16fbe9e94830fa6e18929ad1d
Amara Lovato <amaracuenca@gmail.com>
2017-01-22 13:44:16 -0700

    adds sql and tasks display

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -8,12 +8,17 @@ class TaskManagerApp < Sinatra::Base
   end
 
   get '/tasks' do
-    @tasks = ['task1', 'task2', 'task3']
+    @tasks = Task.all
     erb :index
   end
 
   get '/tasks/new' do
     erb :new
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
   end
 
   post '/tasks' do
